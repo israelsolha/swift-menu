1ec88a29ce1fecb90d8f5d656c03acb63df7974e
Israel Solha <israelsolha@gmail.com>
2024-07-17 22:19:28 -0300

    finishing home_handler_test

--- internal/app/handlers/home_handler_test.go
+++ internal/app/handlers/home_handler_test.go
@@ -1,9 +1,11 @@
 package handlers_test
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"swift-menu-session/internal/domain/entities"
 	"swift-menu-session/testutils"
 	"testing"
 )
@@ -38,6 +40,70 @@ func TestHomeBeingLogged(t *testing.T) {
 	a := testutils.PreapreIntegrationTest()
 	defer testutils.TearDown(a)
 
+	email := "test@test.com"
+
+	user := entities.User{
+		Email:          email,
+		ProfilePicture: "http://mypicture.com",
+		Name:           "Israel Solha",
+	}
+
+	expectedBody := fmt.Sprintf(`<html><body>
+		Welcome %s! You are logged in.<br>
+		<img src="%s" alt="Description of Image" /><br>
+		%s<br>
+		<a href="/protected">Protected Endpoint</a><br>
+		<a href="/logout">Logout</a>
+		</body></html>`,
+		user.Name, user.ProfilePicture, user.Email)
+
+	_, err := a.UserGateway.CreateUser(user)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://localhost:8077", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session, _ := a.SessionCookieStore.GetCookie(req)
+	session.Values["authenticated"] = true
+	session.Values["email"] = email
+	session.Save(req, w)
+
+	cookies := w.Result().Cookies()
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("unexpected status code %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != expectedBody {
+		t.Fatalf("unexpected body returned: %s", body)
+	}
+
+}
+
+func TestHomeBeingLoggedWithoutUser(t *testing.T) {
+	a := testutils.PreapreIntegrationTest()
+	defer testutils.TearDown(a)
+
 	w := httptest.NewRecorder()
 	client := &http.Client{}
 
@@ -64,5 +130,4 @@ func TestHomeBeingLogged(t *testing.T) {
 	if res.StatusCode != 500 {
 		t.Fatalf("unexpected status code %d", res.StatusCode)
 	}
-
 }
